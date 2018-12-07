e53858477ed1e0faedc29c9a7468607e103f5185
Autumn Kelley Barr <autumnkelleyfabulous@Autumns-iMac.local>
2018-12-06 22:56:50 -0700

    Psychic-Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,93 +1,48 @@
-$(document).ready(function() {
-    
+
+
     var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-    var WINS = 0;
-    var Losses = 0;
-    var GuessesLeft = 10;
+    var wins = 0;
+    var losses = 0;
+    var guessesLeft = 10;
     var guessesMade = [];
-    var eachofLetters = null;
+    var userGuess = null;
+    var letterChoices = letters[Math.floor(Math.random() * letters.length)];
 
-    document.onkeyup = function(event) {
+    document.onkeyup = function (event) {
+        console.log('COMPUTER GUESS: ' + letterChoices);
         // var game = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-        var letterChoices = letters[Math.floor(Math.random() * letters.length)];
-        var answer = game[choice];
-        // var mylength = <answer class="length"> </answer>
-        var userGuess = event.key;
-        
-     function countGuessesLeft() {
-         document.querySelector("#letter").innerHTML = "Guesses Made " = letterUser.join(' ');
-
-     }
-
-     countGuessesLeft(); 
-     var restartgame= function() {
-         GuessesLeft = 10
-         letterUser = []
-         var letterChoices = letters[Math.floor(Math.random() * letters.length)];
-     }
-
-    //  When the user presses a key, it will run the following function..
-     document.onkey = function(event) {
+     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+        console.log('USER GUESS: ' + userGuess);
+     
+    // USER GUESS WRONG
+      if (guessesMade.indexOf(userGuess) < 0 && letters.indexOf(userGuess)  >= 0) {
+         guessesMade [guessesMade.length] = userGuess 
          guessesLeft--;
-         var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-
-         letterUser.push(userGuess);
-         countGuessesLeft();
-         farUserGuesses();
+         console.log('GUESSES LEFT: ' + guessesLeft);
      }
-     if (UserGuess === letterChoices && (GuessesLeft > 0)) {
-       
-        WINS++;
-        document.querySelector("#wins").innerHTML = "WINS: " + wins;
- restart();
+    
+
+    // USER GUESS = letterChoice
+
+      if (userGuess === letterChoices) {
+          alert ("You Win!")
+          wins++;
+          document.getElementById("WINS").innerHTML = "Wins" + wins 
+          guessesLeft = 10
+          guessesMade = [];
+          console.log(wins)
+          letterChoices = letters[Math.floor(Math.random() * letters.length)];
+      }
+
+     if (guessesLeft == 0) {
+         
+        alert ("YOU LOST!")
+         losses++
+         guessesLeft = 10
+         guessesMade = [];
+         console.log(losses)
+         document.getElementById("Losses").innerHTML = "Losses" + losses
+         letterChoices = letters[Math.floor(Math.random() * letters.length)];
+        
      }
-     else if (UserGuess === computerGuess && GuessesLeft > 0) {
-             GuessesLeft--;
-              
-	} 
-	else  {
-		losses++;
-        document.querySelector("#losses").innerHTML = "Losses: " + losses;
-        alert("You LOSE! The letter to guess was " + letterToGuess +"!"); 
-       restartgame();
-	}
-   };		
-
-
-
-Collapse 
-var html = 
-       "<h2>Press ANY letter on the keyboard to see your fate!</h2>" +
-       "<Press ANY letter on the keyboard to see your fate!</p>" +
-       "<p>WINS: " + WINS + "</p>" +
-       "<p>Losses: " + Losses + "</p>" +
-       "<p>Guesses Left: " + GuessesLeft + "</p>" +
-       "<p>Guesses Made: " + GuessesMade.join(", ") + "</p>";
-
-//                GuessesLeft = 10;
-//                guessesMade = [];
-//            }
-
-//            if (userGuess != computerGuess) {
-//                GuessesLeft--;
-//                guessesMade.push(userGuess);
-//            }
-
-//            if (GuessesLeft === 0) {
-
-//            GuessesLeft = 9;
-//            Losses ++;
-//            guessesMade = [];
-
-           
-//        }
-
-       
-//        document.querySelector("#game").innerHTML = html;
-
-       
-//        }
-//    };
-
-
-
+    }
