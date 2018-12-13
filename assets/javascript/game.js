16240f6d29d37fee0f71454dfac8b1ef22e7a8b8
Autumn Kelley Barr <autumnkelleyfabulous@Autumns-iMac.local>
2018-12-12 17:56:17 -0700

    Psychicgameadded

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,49 +1,54 @@
 
 
-    var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-    var wins = 0;
-    var losses = 0;
-    var guessesLeft = 10;
-    var guessesMade = [];
-    var userGuess = null;
-    var letterChoices = letters[Math.floor(Math.random() * letters.length)];
-
-    document.onkeyup = function (event) {
-        console.log('COMPUTER GUESS: ' + letterChoices);
-        // var game = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-        console.log('USER GUESS: ' + userGuess);
-     
-    // USER GUESS WRONG
-      if (guessesMade.indexOf(userGuess) < 0 && letters.indexOf(userGuess)  >= 0) {
-         guessesMade[guessesMade.length] = userGuess 
-        //  document.getElementById(guessesMade).innerhtml = "Guesses Made: " + userGuess
-         guessesLeft--;
-         console.log('GUESSES LEFT: ' + guessesLeft);
-     }
-    
-
-    // USER GUESS = letterChoice
-
-      if (userGuess === letterChoices) {
-          alert ("You Win!")
-          wins++;
-          document.getElementById("wins").innerHTML = "Wins" + wins 
-          guessesLeft = 10
-          guessesMade = [];
-          console.log(wins)
-          letterChoices = letters[Math.floor(Math.random() * letters.length)];
-      }
-
-     if (guessesLeft == 0) {
-         
-        alert ("YOU LOST!")
-         losses++
-         guessesLeft = 10
-         guessesMade = [];
-         console.log(losses)
-         document.getElementById("losses").innerHTML = "Losses" + losses
-         letterChoices = letters[Math.floor(Math.random() * letters.length)];
-        
-     }
-    }
+   var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ];
+   var wins = 0;
+   var losses = 0;
+   var guessesLeft = 10;
+   var guessesMade = [];
+   var userGuess = null;
+   var letterChoices = letters[Math.floor(Math.random() * letters.length)];
+   
+   document.onkeyup = function (event) {
+       console.log('COMPUTER GUESS: ' + letterChoices);
+       // var game = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+       var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+       console.log('USER GUESS: ' + userGuess);
+   
+       // USER GUESS WRONG
+       if (guessesMade.indexOf(userGuess) < 0 && letters.indexOf(userGuess) >= 0) {
+           guessesMade[guessesMade.length] = userGuess
+           guessesLeft--;
+           document.getElementById("guessesMade").innerHTML = "Guesses Made: " + guessesMade
+           document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+           console.log('GUESSES LEFT: ' + guessesLeft);
+       }
+   
+   
+       // USER GUESS = letterChoice
+   
+       if (userGuess === letterChoices) {
+           alert("You Win!")
+           wins++;
+           guessesLeft = 10
+           guessesMade = [];
+           console.log(wins)
+           document.getElementById("wins").innerHTML = "Wins: " + wins
+           letterChoices = letters[Math.floor(Math.random() * letters.length)];
+       }
+   
+       if (guessesLeft == 0) {
+   
+           alert("YOU LOST!")
+           losses++
+           guessesLeft = 10
+           guessesMade = [];
+           console.log(losses)
+           document.getElementById("losses").innerHTML = "Losses: " + losses
+           letterChoices = letters[Math.floor(Math.random() * letters.length)];
+   
+       }
+   }
+   
+   
+   
+   Collapse 
